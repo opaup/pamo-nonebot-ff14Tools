6d9471a1ab2cb794719fc7265e3ad742da7767ac
opaup <opaup5259@gmail.com>
2023-09-20 15:41:11 +0800

    no message

--- core/market.py
+++ core/market.py
@@ -4,8 +4,8 @@ noOneSale = '没有找到关于【{}】的市场信息\n宋娜？居然没人卖
 noSellableItems = ''
 existedRelatedItems = '\n你可能想要搜索：{}'
 
+
 async def searchPrice(cmds):
-    
     # 初始化
     length = len(cmds)
     itemName = ''
@@ -23,35 +23,36 @@ async def searchPrice(cmds):
         itemName = itemName[:-2]
         ifHQ = 'true'
     items = await sendCore.searchItem(item=itemName)
-    if len(items) == 0 :
+    if len(items) == 0:
         return '没有找到关于【' + itemName + '】的可售卖道具'
-    result.append('查询【'+ dateCenter +'】中【' + items[0]['Name'] + '】的市场价格：\n')
+    result.append('查询【' + dateCenter + '】中【' + items[0]['Name'] + '】的市场价格：\n')
 
     # 开始查询市场
     itemId = items[0]['ID']
     extPath = dateCenter + '/' + str(itemId) + '/'
     data = await sendCore.doSend(extPath, ifHQ)["listings"]
-    if len(data) == 0 :
-        return noOneSale % (itemName)
+    if len(data) == 0:
+        return noOneSale % itemName
     i = 0
     for item in data:
-        i+=1
+        i += 1
         result.append(str(i))
         result.append('.' + items[0]['Name'])
-        if item['hq'] == True:
+        if item['hq']:
             result.append('(★)')
         result.append('(' + str(item['quantity']) + 'x' + str(item['pricePerUnit']) + ' = ' + str(item['total']) + ')')
         result.append(' 服务器：' + item['worldName'])
         result.append(' 雇员：' + item['retainerName'])
-        if i != len(data) :
+        if i != len(data):
             result.append('\n')
-    if len(items)>=2:
+    if len(items) >= 2:
         otherItems = ''
         for item in items:
             otherItems += ' ' + item['Name']
         result.append('\n你可能想要搜索：' + otherItems)
 
     return "".join(result)
+
 
 async def searchInfo(cmds):
     # 初始化
@@ -70,16 +71,16 @@ async def searchInfo(cmds):
         itemName = itemName[:-2]
         ifHQ = 'true'
     items = await sendCore.searchItem(item=itemName)
-    if len(items) == 0 :
+    if len(items) == 0:
         return '没有找到关于【' + itemName + '】的可售卖道具'
-    result.append('查询【'+ dateCenter +'】中【' + items[0]['Name'] + '】的市场信息：\n')
+    result.append('查询【' + dateCenter + '】中【' + items[0]['Name'] + '】的市场信息：\n')
     # 开始查询市场
     itemId = items[0]['ID']
     extPath = dateCenter + '/' + str(itemId) + '/'
     data = await sendCore.doSend(extPath, ifHQ)
-    if len(data["listings"]) == 0 :
+    if len(data["listings"]) == 0:
         return noOneSale % (itemName)
-    
+
     # 未售平均价格（NQ+HQ）
     currentAveragePrice = data['currentAveragePrice']
     # 未售平均价格（NQ
@@ -110,8 +111,5 @@ async def searchInfo(cmds):
     maxPriceNQ = data['maxPriceNQ']
     # 最大销售单价（HQ
     maxPriceHQ = data['maxPriceHQ']
-
-
-
 
     return
