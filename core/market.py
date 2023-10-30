97cf68e0f21849a03960dd65a1ef05d749acac29
opaup <opaup5259@gmail.com>
2023-10-30 20:03:54 +0800

    更新了当前pvp地图查询

--- core/market.py
+++ core/market.py
@@ -73,43 +73,64 @@ async def searchInfo(cmds):
     items = await sendCore.searchItem(item=itemName)
     if len(items) == 0:
         return '没有找到关于【' + itemName + '】的可售卖道具'
-    result.append('查询【' + dateCenter + '】中【' + items[0]['Name'] + '】的市场信息：\n')
+    result.append('查询【' + dateCenter + '】中【' + items[0]['Name'] + '】的市场历史行情：\n')
     # 开始查询市场
     itemId = items[0]['ID']
     extPath = dateCenter + '/' + str(itemId) + '/'
     data = await sendCore.doSend(extPath, ifHQ)
     if len(data["listings"]) == 0:
-        return noOneSale % (itemName)
+        return noOneSale % itemName
 
     # 未售平均价格（NQ+HQ）
     currentAveragePrice = data['currentAveragePrice']
-    # 未售平均价格（NQ
+    result.append("未售平均价格（NQ+HQ）：" + currentAveragePrice)
+    # 未售平均价格（NQ）
     currentAveragePriceNQ = data['currentAveragePriceNQ']
-    # 未售平均价格（HQ
+    result.append("\n未售平均价格（NQ）：" + currentAveragePriceNQ)
+    # 未售平均价格（HQ）
     currentAveragePriceHQ = data['currentAveragePriceHQ']
+    result.append("\n未售平均价格（HQ）：" + currentAveragePriceHQ)
     # 平均日售量
     regularSaleVelocity = data['regularSaleVelocity']
+    result.append("\n平均日售量：" + regularSaleVelocity)
     # NQ 售卖速度
     nqSaleVelocity = data['nqSaleVelocity']
+    result.append("\nNQ 售卖速度：" + nqSaleVelocity)
     # HQ 售卖速度
     hqSaleVelocity = data['hqSaleVelocity']
+    result.append("\nHQ 售卖速度：" + hqSaleVelocity)
     # 已售平均价格（NQ+HQ）
     averagePrice = data['averagePrice']
-    # 已售平均价格（NQ
+    result.append("\n已售平均价格（NQ+HQ）：" + averagePrice)
+    # 已售平均价格（NQ）
     averagePriceNQ = data['averagePriceNQ']
-    # 已售平均价格（HQ
+    result.append("\n已售平均价格（NQ）：" + averagePriceNQ)
+    # 已售平均价格（HQ）
     averagePriceHQ = data['averagePriceHQ']
+    result.append("\n已售平均价格（HQ）：" + averagePriceHQ)
     # 最小销售单价（NQ+HQ）
     minPrice = data['minPrice']
-    # 最小销售单价（NQ
+    result.append("\n最小销售单价（NQ+HQ）：" + minPrice)
+    # 最小销售单价（NQ）
     minPriceNQ = data['minPriceNQ']
-    # 最小销售单价（HQ
+    result.append("\n最小销售单价（NQ）：" + minPriceNQ)
+    # 最小销售单价（HQ）
     minPriceHQ = data['minPriceHQ']
+    result.append("\n最小销售单价（HQ）：" + minPriceHQ)
     # 最大销售单价（NQ+HQ）
     maxPrice = data['maxPrice']
-    # 最大销售单价（NQ
+    result.append("\n最大销售单价（NQ+HQ）：" + maxPrice)
+    # 最大销售单价（NQ）
     maxPriceNQ = data['maxPriceNQ']
-    # 最大销售单价（HQ
+    result.append("\n最大销售单价（NQ）：" + maxPriceNQ)
+    # 最大销售单价（HQ）
     maxPriceHQ = data['maxPriceHQ']
+    result.append("\n最大销售单价（HQ）：" + maxPriceHQ)
+
+    if len(items) >= 2:
+        otherItems = ''
+        for item in items:
+            otherItems += ' ' + item['Name']
+        result.append('\n你可能想要搜索：' + otherItems)
 
     return
